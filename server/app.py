70a5bca1d6dead72cb3e230a24fbdd916b954741
Johan Nilsson <github@markupartist.com>
2012-09-06 16:59:41 +0200

    Add proxy for google maps.

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from bottle import route, run, debug, Bottle, request, abort, static_file
 from channel.handler import channel_app
+import requests
 
 app = Bottle()
 app.mount('/channel', channel_app)
@@ -7,6 +8,12 @@ app.mount('/channel', channel_app)
 @app.route('/')
 def root():
     return "Hyyyyyper"
+
+@app.route('/geocode/:name:')
+def geocode(name):
+    u = 'http://maps.googleapis.com/maps/api/geocode/json?address={0}&sensor=true'.format(name)
+    r = requests.get(u)
+    return r.json
 
 def main():
     run(reloader=True, app=app, host='localhost', port=8888)
